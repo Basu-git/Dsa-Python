1688122b65fca2820f0bb7bdd406e638075c06c9
Basu <basavarajumachagi99@gmail.com>
2026-03-21 14:05:13 +0530

    Palindromme check

---
+++ Recursion/palindrome.py
@@ -0,0 +1,4 @@
+n=input("Enter the string: ")
+def palindrome(n):
+    return(n[::-1]==n)
+print(palindrome(n))
--- Recursion/reverse_arr.py
+++ Recursion/reverse_arr.py
@@ -5,7 +5,7 @@ class Solution:
         while left<right:
             arr[left],arr[right]=arr[right],arr[left]
             left+=1
-
+            right-=1
 
 if __name__ == "__main__":
     arr = [1, 2, 3, 4, 5]
